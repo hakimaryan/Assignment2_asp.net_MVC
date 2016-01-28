1ab883171469631ecfb6d4d0dca41e84707f0db9
Hakim <u1367698@unimail.hud.ac.uk>
2016-01-28 21:23:40 +0000

    contact changed

---
+++ asp.net_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="asp.net_MVC.MvcApplication" Language="C#" %>
---
+++ asp.net_MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(asp.net_MVC.Startup))]
+namespace asp.net_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
