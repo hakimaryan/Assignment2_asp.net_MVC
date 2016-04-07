bf2c0f62ee31cba0343e360e02b921545987f341
ukhak <ukhak@hotmail.com>
2016-04-07 12:02:17 +0100

    codes broken

---
+++ asp.net_MVC/Models/Pet.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Web.Mvc.Html;
+using System.ComponentModel.DataAnnotations;
+
+namespace asp.net_MVC.Models
+{
+    public enum PetTypes
+    {
+        Cat, Dog, Rabbit, Snake, Ferret, Lizard, Others
+    }
+
+    public class Pet
+    {
+        public int petId { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Pet Name")]
+        public string petName { get; set; }
+
+        [Display(Name = "Pet Type")]
+        [DisplayFormat(NullDisplayText = "Select the Pet Type")]
+        public PetTypes? petTypes { get; set; }
+        [DataType(DataType.Date)]
+        //The DisplayFormat attribute is used to explicitly specify the date format
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Missing Date")]
+        public DateTime missingDate { get; set; }
+        public string Description { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal Reward { get; set; }
+    }
+
+    public class PetDBContext : DbContext
+    {
+        public DbSet<Pet> Pets { get; set; }
+
+        public System.Data.Entity.DbSet<asp.net_MVC.Models.Pet> PetOwners { get; set; }
+    }
+
+}
