1c6bb9ae7020be0d14647eb026ca892fae469236
hakim <u1367698@unimail.hud.ac.uk>
2016-04-12 06:45:07 +0100

    Removed unused  namespaces

--- asp.net_MVC/DataContexts/PetDBContext.cs
+++ asp.net_MVC/DataContexts/PetDBContext.cs
@@ -1,20 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data.Entity;
-using System.ComponentModel.DataAnnotations;
-using Microsoft.AspNet.Identity;
+﻿using System.Data.Entity;
 using asp.net_MVC.Models;
 
 namespace asp.net_MVC.DataContexts
 {
     class PetDBContext : DbContext
     {
+        public PetDBContext()
+           : base("DefaultConnection")
+        { }
         public DbSet<Pet> Pets { get; set; }
         public DbSet<File> Files { get; set; }
         public DbSet<FilePath> FilePaths { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
 
     }
 }
